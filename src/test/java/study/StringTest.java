622c4099537dbf196c0279809a44a5e61c3d30ae
DoubleK <airvw14@gmail.com>
2022-05-12 22:52:23 +0900

    substring(0,4) to substring(1,4) @DisplayName added

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,10 +1,16 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
+@DisplayName("String")
 public class StringTest {
+
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
@@ -15,29 +21,33 @@ public class StringTest {
     void split(){
         String[] actual = "1,2".split(",");
         String[] actualExactly = "1".split(",");
-        assertThat(actual).contains("1", "2");
+        assertThat(actual).contains("1","2");
         assertThat(actualExactly).containsExactly("1");
     }
 
     @Test
     void substring(){
-        String actual = "(1,2)".substring(0,4);
+        String actual = "(1,2)".substring(1,4);
         assertThat(actual).contains("1,2");
     }
 
     @Test
-    void charAt(){
-        char actual0 = "abc".charAt(0);
-        char actual1 = "abc".charAt(1);
-        char actual2 = "abc".charAt(2);
-        int index = 5;
+    @DisplayName("\"abc\" charAt Test with StringIndexOutOfBoundsException")
+    void charAt() {
+        String str = "abc";
+        char actual0 = str.charAt(0);
+        char actual1 = str.charAt(1);
+        char actual2 = str.charAt(2);
+        int outOfIndex = 5;
         assertThat(actual0).isEqualTo('a');
         assertThat(actual1).isEqualTo('b');
         assertThat(actual2).isEqualTo('c');
-        assertThatThrownBy(() -> {char actualError = "abc".charAt(index);}).isInstanceOf(IndexOutOfBoundsException.class)
-                .hasMessageContaining("%d", index);
-//        assertThatExceptionOfType(IndexOutOfBoundsException.class).isThrownBy(() -> {char actualError = "abc".charAt(index);})
-//                .withMessageMatching("Index: \\d+, Size: \\d+", index, "abc".length());
-
+        assertThatThrownBy(() -> {
+            char actualError = str.charAt(outOfIndex);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("range: %d", outOfIndex);
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> {char actualError = str.charAt(outOfIndex);})
+                .withMessage("String index out of range: %d", outOfIndex);
     }
 }
