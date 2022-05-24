88d7dcef674bb2cbf8a082029e46a2fed1323e6c
DoubleK <airvw14@gmail.com>
2022-05-24 22:07:31 +0900

    SetTest 연습

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,16 +1,15 @@
 package study;
 
-import com.sun.tools.javac.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.*;
-
 import java.util.HashSet;
 import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SetTest {
     private Set<Integer> numbers;
