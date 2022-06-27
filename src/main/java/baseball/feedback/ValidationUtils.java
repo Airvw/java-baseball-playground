68d2ec06aaf493fd4e148ac86597f6dea83cbf52
DoubleK <airvw14@gmail.com>
2022-06-27 21:12:56 +0900

    refactor(refactoring): utils refactoring
    refactoring number 1 to 9 validation test
    
    BREAKING CHANGE: change ValidationUtils.java
    - validNum method if
    statement simplify
    
    [skip ci]

--- src/main/java/baseball/feedback/ValidationUtils.java
+++ src/main/java/baseball/feedback/ValidationUtils.java
@@ -3,8 +3,6 @@ package baseball.feedback;
 public class ValidationUtils {
 
     public static boolean validNum(int num) {
-        if(num >= 1 && num <= 9) return true;
-
-        return false;
+        return num >= 1 && num <= 9;
     }
 }
