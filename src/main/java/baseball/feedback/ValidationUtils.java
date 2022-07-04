b3195a639f4715c8034c0dc48af491818ce9abcd
DoubleK <airvw14@gmail.com>
2022-07-04 22:39:12 +0900

    test(utils): utilss test
    make test for generate number using validNum method
    make test for checking different number from random ballnumber
    
    BREAKING CHANGE: change ValidationBallsTest.java
    - new isNotSameNumTest method
    
    change ValidationUtils.java
    - new generateNum method
    - new isNotSameNum method
    
    [skip ci]

--- src/main/java/baseball/feedback/ValidationUtils.java
+++ src/main/java/baseball/feedback/ValidationUtils.java
@@ -1,5 +1,7 @@
 package baseball.feedback;
 
+import java.util.*;
+
 public class ValidationUtils {
     private static final int MIN_NUM = 1;
     private static final int MAX_NUM = 9;
@@ -7,4 +9,16 @@ public class ValidationUtils {
     public static boolean validNum(int num) {
         return num >= MIN_NUM && num <= MAX_NUM;
     }
+
+    public static boolean isNotSameNum(List<Integer> randomBall, int randomNum) {
+        return randomBall.stream()
+                .noneMatch(ballNum -> ballNum == randomNum);
+    }
+
+    public static int generateNum() {
+        int num = (int)(Math.random() * 10);
+        if(validNum(num)) return num;
+        return generateNum();
+    }
+
 }
--- src/test/java/baseball/feedback/ValidationUtilsTest.java
+++ src/test/java/baseball/feedback/ValidationUtilsTest.java
@@ -1,11 +1,23 @@
 package baseball.feedback;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import java.util.*;
 
 public class ValidationUtilsTest {
+
+    private List<Integer> randomBall;
+    private int randomNum1 = 9;
+    private int randomNum2 = 2;
+
+    @BeforeEach
+    void setUp(){
+        randomBall = new ArrayList<>();
+        randomBall.add(9);
+    }
 
     @Test
     @DisplayName("check 1 to 9 number")
@@ -14,5 +26,12 @@ public class ValidationUtilsTest {
         assertThat(ValidationUtils.validNum(1)).isTrue();
         assertThat(ValidationUtils.validNum(0)).isFalse();
         assertThat(ValidationUtils.validNum(10)).isFalse();
+    }
+
+    @Test
+    @DisplayName("서로 다른 수 인지 확인")
+    void isNotSameNumTest(){
+        assertThat(ValidationUtils.isNotSameNum(randomBall, randomNum1)).isFalse();
+        assertThat(ValidationUtils.isNotSameNum(randomBall, randomNum2)).isTrue();
     }
 }
